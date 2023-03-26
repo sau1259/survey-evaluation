01213c9b5cc00547088c5f4bd4a9968563ffd079
sau1259 <85556724+sau1259@users.noreply.github.com>
2023-03-26 16:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ final_table = pd.DataFrame()
 
 # -------------------------------- Selenium Code for Automation ----------------------------------------------
 
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+CHROMEDRIVER_PATH = '.streamlit/chromedriver'
 #CHROMEDRIVER_PATH = ChromeDriverManager().install()
 
 # Load Required drivers and services
